c57354934afa014c2df0eb49aa01502a99e4694f
Northword <44738481+northword@users.noreply.github.com>
2021-04-27 21:10:22 +0800

    add typora themes

---
+++ Typora/mklink4typora.bat
@@ -0,0 +1,5 @@
+@echo off
+REM mklink /D "D:\Tools\Clash for Windows\data" "D:\Code\Scripts\Clash"
+mklink /D "C:\Users\Northword\AppData\Roaming\Typora\themes" "D:\Code\Scripts\Typora\themes" 
+
+pause
---
+++ VSCode/mklink4vscode.bat
@@ -0,0 +1,4 @@
+@echo off
+mklink /D "C:\Users\Northword\AppData\Roaming\Code\User" "D:\Code\Scripts\VSCode\User" 
+
+pause
